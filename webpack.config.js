7cc8d1404c243e33779b941f7b0bc9a7c3750c85
Ante Tomić <A.Tomic@infobip.com>
2019-02-17 00:03:13 +0100

    Modify development server by defining it inside of configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,10 @@ module.exports = {
             title: "Webpack .NET meetup",
         }),
     ],
+    devServer: {
+        stats: 'errors-only',
+        host: process.env.HOST, // Defaults to localhost
+        port: process.env.PORT, // Defaults to 8080
+        open: true, // Open the page in browser
+    },
 };
