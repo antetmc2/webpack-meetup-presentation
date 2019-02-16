ae5a15816c0d8dbfcbd9b734505d775715721bae
Ante Tomić <A.Tomic@infobip.com>
2019-02-17 00:10:04 +0100

    Playing with webpack-dev-server features a little bit :)

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,6 @@ module.exports = {
         host: process.env.HOST, // Defaults to localhost
         port: process.env.PORT, // Defaults to 8080
         open: true, // Open the page in browser
+        //writeToDisk: true, // If we e.g. wanna see our output in dist/ folder, uncomment if you wanna try it
     },
 };
